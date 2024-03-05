8e0cfb078565fe9dc381f8042afc16d4fc6bd310
whitef19 <39767591+whitef19@users.noreply.github.com>
2024-03-05 10:09:26 -0500

    remove PC by default

--- sv-analysis.R
+++ sv-analysis.R
@@ -52,24 +52,25 @@ names(SVs) = paste0("SV",1:ncol(SVs))
 ## concatenate SVs to covariate dataframe 
 clean_design_df = cbind(clean_design_df, SVs)
 
+# if PCA is more appropriate :
 ### Principal Component Analysis
-PCs = principal_component_analysis(normalization_obj$Voom$E)
+#PCs = principal_component_analysis(normalization_obj$Voom$E)
 ## concatenate PCs to covariate dataframe 
-clean_design_df = cbind(clean_design_df, PCs)
+#clean_design_df = cbind(clean_design_df, PCs)
 
 
 write.table(cbind("ID"=rownames(clean_design_df), clean_design_df), file=paste0(output_path,"/full_design.tsv"), sep="\t", row.names=F, quote=F)
 
-
+### evaluate SVs 
 included_covariate_list = c(full_model_list, names(SVs))
 create_corrplot(clean_design_df[, included_covariate_list], "SV")
 latent_variable_evaluation(normalization_obj$Voom$E, clean_design_df[, included_covariate_list], "SV")
 
-included_covariate_list = c(full_model_list, names(PCs))
-create_corrplot(clean_design_df[, included_covariate_list], "PC")
-latent_variable_evaluation(normalization_obj$Voom$E, clean_design_df[, included_covariate_list], "PC")
+#included_covariate_list = c(full_model_list, names(PCs))
+#create_corrplot(clean_design_df[, included_covariate_list], "PC")
+#latent_variable_evaluation(normalization_obj$Voom$E, clean_design_df[, included_covariate_list], "PC")
 
-included_covariate_list = c(full_model_list, names(SVs), names(PCs))
-create_corrplot(clean_design_df[, included_covariate_list], "SV_and_PC")
+#included_covariate_list = c(full_model_list, names(SVs), names(PCs))
+#create_corrplot(clean_design_df[, included_covariate_list], "SV_and_PC")
 
 cat(paste0("##### All done !: ",Sys.Date(), " at ", Sys.time(),"\n"), file=log_file)
